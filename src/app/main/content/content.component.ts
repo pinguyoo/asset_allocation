59b68ef75de93f177d0a70458646a0231064b8ff
ben <ben@localhost.localdomain>
2022-03-13 11:10:08 -0400

    add content and routing

--- src/app/main/content/content.component.ts
+++ src/app/main/content/content.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  get title(): string {
+    return this.router.url.split('/')?.[1].toUpperCase();
   }
 
+  constructor(private readonly router: Router) {
+    console.log(this.router);
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/main/content/content.module.ts
+++ src/app/main/content/content.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentComponent } from './content.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { ContentRoutingModule } from './content-routing.module';
 
 @NgModule({
-  declarations: [
-    ContentComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardModule,
-  ],
+  declarations: [ContentComponent],
+  imports: [CommonModule, ContentRoutingModule, DashboardModule],
   exports: [ContentComponent],
 })
-export class ContentModule { }
+export class ContentModule {}
